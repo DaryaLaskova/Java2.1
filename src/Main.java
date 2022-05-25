da3b3af29b9a8b2af079534e2fd864f08cfed5ef
DaryaLaskova <Daryalaskova@inbox.ru>
2022-05-25 21:31:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1050;
+        int b = 20;
+
+        System.out.println("Начислено миль за покупку: " + (a / b));
+
+
+    }
+}
